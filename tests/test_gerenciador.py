9bd22ea71df13eefa30a25b275c3c47fbaa76096
Bruna Garais <brunagarais@yahoo.com.br>
2025-06-08 23:45:15 -0300

    test: adiciona estrutura inicial de testes automatizados

---
+++ tests/test_gerenciador.py
@@ -0,0 +1,8 @@
+import unittest
+
+class TestGerenciadorBiblioteca(unittest.TestCase):
+    def test_exemplo(self):
+        self.assertEqual(2 + 2, 4)
+
+if __name__ == '__main__':
+    unittest.main()
